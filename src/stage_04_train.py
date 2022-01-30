7279b3926f3e1f07a6f9b1f7eadedb03d9c25e05
kul <kirticse.chakma869@gmail.com>
2022-01-30 04:50:20 +0100

    stage_04 model and callbacks loaded

--- src/stage_04_train.py
+++ src/stage_04_train.py
@@ -1,4 +1,6 @@
 from src.utils.all_utils import read_yaml, create_directory
+from src.utils.models import load_full_model
+from src.utils.callbacks import get_callbacks
 import argparse
 import os
 import logging
@@ -16,13 +18,17 @@ def train_model(config_path,params_path):
     params=read_yaml(params_path)
     artifacts=config["artifacts"]
     artifacts_dir=artifacts["ARTIFACTS_DIR"]
-    tensorboard_log_dir=os.path.join(artifacts_dir,artifacts["TENSORBOARD_ROOT_LOG_DIR"])
-    checkpoint_dir=os.path.join(artifacts_dir,artifacts["CHECKPOINT_DIR"])
-    callbacks_dir=os.path.join(artifacts_dir,artifacts["CALLBACKS_DIR"])
-    create_directory([tensorboard_log_dir,checkpoint_dir,callbacks_dir])
+    train_model_dir_path=os.path.join(artifacts_dir,artifacts
+    ["TRAINED_MODEL_DIR"])
+    create_directory([train_model_dir_path])
+    untrained_full_model_path=os.path.join(artifacts_dir,artifacts
+    ["BASE_MODEL_DIR"],artifacts["UPDATED_BASE_MODEL_NAME"])
+    model=load_full_model(untrained_full_model_path)
+    callback_dir_path=os.path.join(artifacts_dir,artifacts["CALLBACKS_DIR"])
+    callbacks=get_callbacks(callback_dir_path)
+    
 
-    create_and_save_tensorboard_callback(callbacks_dir,tensorboard_log_dir)
-    create_and_save_checkpoint_callback(callbacks_dir,checkpoint_dir)
+    
 
 
 
