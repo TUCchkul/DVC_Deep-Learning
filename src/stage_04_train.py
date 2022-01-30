43da2616426bcf9d3006a6fb099443c8c8a34405
kul <kirticse.chakma869@gmail.com>
2022-01-30 04:00:31 +0100

    stage_four  base file created

---
+++ src/stage_04_train.py
@@ -0,0 +1,43 @@
+from src.utils.all_utils import read_yaml, create_directory
+import argparse
+import os
+import logging
+import io
+
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir, 'running_logs.log'), level=logging.INFO, format=logging_str,
+                    filemode="a")
+
+
+def train_model(config_path,params_path):
+    config=read_yaml(config_path)
+    params=read_yaml(params_path)
+    artifacts=config["artifacts"]
+    artifacts_dir=artifacts["ARTIFACTS_DIR"]
+    tensorboard_log_dir=os.path.join(artifacts_dir,artifacts["TENSORBOARD_ROOT_LOG_DIR"])
+    checkpoint_dir=os.path.join(artifacts_dir,artifacts["CHECKPOINT_DIR"])
+    callbacks_dir=os.path.join(artifacts_dir,artifacts["CALLBACKS_DIR"])
+    create_directory([tensorboard_log_dir,checkpoint_dir,callbacks_dir])
+
+    create_and_save_tensorboard_callback(callbacks_dir,tensorboard_log_dir)
+    create_and_save_checkpoint_callback(callbacks_dir,checkpoint_dir)
+
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+
+    args.add_argument("--config", "-c", default="config/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+
+    parsed_args = args.parse_args()
+
+    try:
+        logging.info(">>>>> stage four started")
+        train_model(config_path=parsed_args.config, params_path=parsed_args.params)
+        logging.info("stage four completed! training completed and model is saved>>>>>\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
